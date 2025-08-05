da91059d7f2d20e4814fab1347ed35a96d430da3
Sanjay M <sanjay@securden.com>
2025-08-05 17:40:37 +0530

    test secret 2

--- Secret.py
+++ Secret.py
@@ -5,3 +5,5 @@ Testsecret = "AIzaSyDdAWF9J9YgSE3GyXg_hFrUTugba4vqQhM"
 dummy = "AIzaSyDjliKaJFdyh10_8YeTfQavNVRcY9SHhyI"
 
 deumm1 = "AIzaSyAaY7qJwsV_uag37VJ-LYO9qqFUh42G0qE"
+
+mysecret = "MySecret"
