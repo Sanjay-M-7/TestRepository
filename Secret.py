954f116c021d5e12a544c4dd9e4d6a4815cca3f6
Sanjay-M-7 <102400952+Sanjay-M-7@users.noreply.github.com>
2025-08-05 17:18:15 +0530

    Update1 Secret.py

--- Secret.py
+++ Secret.py
@@ -3,3 +3,5 @@ Secret = "AIzaSyBzGiqpzPYGJx-bANrV4zd-CG2fvf1sGZ0"
 Testsecret = "AIzaSyDdAWF9J9YgSE3GyXg_hFrUTugba4vqQhM"
 
 dummy = "AIzaSyDjliKaJFdyh10_8YeTfQavNVRcY9SHhyI"
+
+deumm1 = "AIzaSyAaY7qJwsV_uag37VJ-LYO9qqFUh42G0qE"
